87df1f23851e470dcfbbdce0282857639b9a1f73
danielw12 <146634903+dwnuk@users.noreply.github.com>
2024-04-09 13:40:22 +0200

    another webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,143 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
+const PATHS = {
+    src: path.join(__dirname, 'src')
+}
 
-const stylesHandler = 'style-loader';
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const fileExtension = process.env.NODE_ENV === 'production' ? 'html' : 'html';
+const dataCoreFile = process.env.NODE_ENV === 'production' ? 'cleanFile' : 'dataCoreDev';
 
-const config = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].bundle.js',
-    assetModuleFilename: 'assets/[hash][ext][query]'
-  },
-  devServer: {
-    open: true,
-    host: 'localhost',
-    watchFiles: ['./src/**/*']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new CleanWebpackPlugin(),
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset/resource',
-      },
+module.exports = {
+    mode: mode,
 
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+    devServer: {
+        open: true,
+        watchFiles: ['./src/**/*']
+    },
+
+    entry: {
+        index: "./src/js/index.js",
+    },
+
+    output: {
+        filename: "[name].[chunkhash].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].min.css"
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: `index.html`,
+            chunks: ['index', `${dataCoreFile}`]
+        }),
+
+        // new CopyWebpackPlugin({
+        //     patterns: [{
+        //         from: "./src/pdf/**/*",
+        //         to: "[name].[ext]",
+        //     }]
+        // }),
+
+        new CleanWebpackPlugin()
     ],
-  },
-};
 
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-  } else {
-    config.mode = 'development';
-  }
-  return config;
+    module: {
+        rules: [
+
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                    "postcss-loader"
+                ],
+            },
+            {
+                test: /\.(pdf|svg|png|jpg|jpeg|gif|mp4|otf|webp|woff2|woff|ttf)$/,
+                use:
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name][contenthash].[ext]",
+                            outputPath: "img/",
+                            esModule: false
+                        }
+                    }
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                include: /data-zoomable/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name][contenthash].[ext]",
+                        outputPath: "img/",
+                        esModule: false
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        esModule: false,
+                        sources: {
+                            list: [
+                                {
+                                    tag: 'source',
+                                    attribute: 'src',
+                                    type: 'src',
+                                },
+                                {
+                                    tag: 'img',
+                                    attribute: 'data-src',
+                                    type: 'src',
+                                },
+                                {
+                                    tag: 'img',
+                                    attribute: 'src',
+                                    type: 'src',
+                                },
+                                {
+                                    tag: 'source',
+                                    attribute: 'srcset',
+                                    type: 'srcset',
+                                },
+                                {
+                                    tag: 'video',
+                                    attribute: 'data-src',
+                                    type: 'src',
+                                },
+                                {
+                                    tag: 'picture',
+                                    attribute: 'data-zoomable',
+                                    type: 'src',
+                                }
+
+                            ]
+                        }
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+
+    devtool: 'source-map' // Add this line to enable source maps
 };
